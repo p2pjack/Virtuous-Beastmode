79682672e621d6d1ae93408c5703a3a18e8389df
p2pjack <p2pjack@gmail.com>
2012-09-27 13:31:57 +0100

    Virtuous-BeastMode
    All credit to team virtuous and team blackout

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Mon Sep 24 12:22:54 BST 2012"
+#define LINUX_COMPILE_BY "p2pjack"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7.2 20120910 (prerelease) (Linaro GCC 4.7-2012.09-1~dev) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.43-virtuous-BeastMode-V1-test-2"
